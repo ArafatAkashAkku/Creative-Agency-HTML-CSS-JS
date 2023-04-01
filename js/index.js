1faff3e5bf22f6e12826c71a543edcb92ed23d08
ArafatAkashAkku <arafatakash5@gmail.com>
2023-04-02 00:22:01 +0600

    added cookies

--- js/index.js
+++ js/index.js
@@ -17,9 +17,9 @@ window.onscroll = () => {
     let cheight = container.offsetHeight - window.innerHeight;
     let cpos = container.getBoundingClientRect();
     let diff = cheight + cpos.top;
-    let progress = diff/cheight*100;
-    let csswidth = Math.floor(100-progress);
-    highlight.style.width=csswidth + "%";
+    let progress = diff / cheight * 100;
+    let csswidth = Math.floor(100 - progress);
+    highlight.style.width = csswidth + "%";
 }
 
 // navigation bar on click effect 
@@ -41,15 +41,47 @@ phoneBar.onclick = () => {
 // navigation bar links on click effect 
 const navLinks = document.querySelectorAll('header .right ul li a')
 
-navLinks.forEach((element)=>{
-    element.onclick=()=>{
-    phoneNav.classList.remove("active")
-    phoneBar.classList.replace("fa-xmark", "fa-bars")
+navLinks.forEach((element) => {
+    element.onclick = () => {
+        phoneNav.classList.remove("active")
+        phoneBar.classList.replace("fa-xmark", "fa-bars")
     }
 })
 
 // right click disable 
 
-document.oncontextmenu=(element)=>{
+document.oncontextmenu = (element) => {
     element.preventDefault();
 }
+
+// cookie set 
+
+const cookieBox = document.querySelector(".cookie");
+const acceptBtn = document.querySelector(".cookie .right .accept");
+const rejectBtn = document.querySelector(".cookie .right .reject");
+
+setTimeout(() => {
+    cookieBox.style.visibility = "visible"
+}, 1000);
+
+rejectBtn.onclick = () => {
+    cookieBox.style.visibility = "hidden";
+    setTimeout(() => {
+        cookieBox.style.visibility = "visible"
+    }, 60000);
+}
+
+acceptBtn.onclick = () => {
+    document.cookie = "fname=Arafat; max-age=" + 60 * 60 * 24 * 30;
+    document.cookie = "lname=Akash; max-age=" + 60 * 60 * 24 * 30;
+    if (document.cookie) {
+        cookieBox.classList.add("hide");
+    }
+    else {
+        alert("Cookie can't be set");
+    }
+}
+
+let check = document.cookie.indexOf("fname=Arafat");
+check != -1 ? cookieBox.classList.add("hide") : cookieBox.classList.remove("hide");
+
